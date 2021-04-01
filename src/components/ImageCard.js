8fc6d7e458ec9ad723b65392e8250dea53246011
Dave-Sama <davech1228@gmail.com>
2021-04-02 00:43:32 +0300

    a bit of a style added

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -20,7 +20,15 @@ export default class ImageCard extends React.Component {
 		const { description, urls } = this.props.image;
 		return (
 			<div style={{ gridRowEnd: `span ${this.state.spans}` }}>
-				<img ref={this.imageRef} alt={description} src={urls.regular} />
+				<img
+					style={{
+						borderRadius: '10px',
+						boxShadow: '0px 0px 9px 5px rgba(0, 1, 255, .2)',
+					}}
+					ref={this.imageRef}
+					alt={description}
+					src={urls.regular}
+				/>
 			</div>
 		);
 	}
